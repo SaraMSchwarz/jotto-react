e2ca7a46f7410377d1f67191078be1ccbbb1b5ed
github username <saramschwarz@gmail.com>
2019-08-23 14:11:02 -0400

    add component component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,13 +1,24 @@
+import React from 'react';
 /**
  *Functional React component for congratulatory message.
  *@Function
+ *@param {object} props - react props
  *@returns {JSX.element} - rendered component if true
 */
-import React from 'react';
 
-export default () => {
-  return (
-    <div>
-    </div>
-  )
+
+export default (props) => {
+    if (props.success) {
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word.
+          </span>
+        </div>
+      )
+  } else {
+    return (
+      <div data-test="component-congrats" />
+    )
+  }
 }
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,10 @@
-export const findByTestAttr = (shallow, val) => {
-  wrapper.find(`[data-test="${val}"]`)
+/**
+* return node(s) with the given data-test attribute.
+* @param {ShallowWrapper} wrapper - enzyme shallow wrapper.
+* @param {string} val - value of data-test attribute for search.
+* @returns {ShallowWrapper}
+*/
+
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
 }
