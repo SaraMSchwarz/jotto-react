e2211fbb93299ff1699450a96a206bbb8ff9f67a
github username <saramschwarz@gmail.com>
2019-08-24 13:55:05 -0400

    set up GuessedWords component and test files

---
+++ src/GuessedWords.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Proptypes from 'prop-types'
+
+const GuessedWords = (props) => {
+  return (
+    <div />
+  )
+}
+
+GuessedWords.propTypes = {
+
+}
+
+export default GuessedWords
